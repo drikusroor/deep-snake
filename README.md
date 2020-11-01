a910c6349be6eb37e51b2a98b525f50694e443aa
Drikus Roor <drikusroor@gmail.com>
2020-11-01 22:29:53 +0100

    Add snake game to use for neural network

---
+++ README.md
@@ -0,0 +1,14 @@
+# Deep Snake
+
+A snake game made in Pygame 2 that will be used to train a neural network AI that will eventually take control over snake.
+
+## Prequisites
+
+- Python 3.8
+- Pygame 2 ( `python3 -m pip install pygame==2.0.0` )
+
+## Run Deep Snake
+
+```sh
+python main.py
+```
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import sys, pygame
+from enum import Enum
+
+
+class Direction(Enum):
+    UP = (1,)
+    RIGHT = (2,)
+    DOWN = (3,)
+    LEFT = 4
+
 
 pygame.init()
+clock = pygame.time.Clock()
+FPS = 4
 
 size = width, height = 320, 240
 speed = [2, 2]
@@ -8,20 +19,100 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+BLOCK_SIZE = 10
+ROWS_AMOUNT = int(height / BLOCK_SIZE)
+COLS_AMOUNT = int(width / BLOCK_SIZE)
+
+game_state = [[0] * COLS_AMOUNT for i in range(ROWS_AMOUNT)]
+direction_state = Direction.LEFT
+snake = []
+
+
+def reset_snake():
+    snake.clear()
+    for r_index, row in enumerate(game_state):
+        if r_index == len(game_state) / 2:
+            head_index = int((COLS_AMOUNT / 2) - 1)
+            snake.append((r_index, head_index))
+            snake.append((r_index, head_index + 1))
+            snake.append((r_index, head_index + 2))
+            snake.append((r_index, head_index + 3))
+            snake.append((r_index, head_index + 4))
+
+
+reset_snake()
+
+
+def move_snake():
+    move_possible = True
+    head = snake[0]
+    if direction_state == Direction.UP:
+        next_move = (head[0] - 1, head[1])
+    elif direction_state == Direction.RIGHT:
+        next_move = (head[0], head[1] + 1)
+    elif direction_state == Direction.DOWN:
+        next_move = (head[0] + 1, head[1])
+    else:
+        next_move = (head[0], head[1] - 1)
+
+    for segment in snake:
+        if segment[0] == next_move[0] and segment[1] == next_move[1]:
+            move_possible = False
+        elif (
+            next_move[0] < 0
+            or next_move[0] > ROWS_AMOUNT - 1
+            or next_move[1] < 0
+            or next_move[1] > COLS_AMOUNT - 1
+        ):
+            move_possible = False
+
+    # Create data sample and label here and send it to train model
+    # Information about game_state (empty = 0, snake = 1, snake_head = 2)
+    # Information about direction
+    # Information about world size? (or is this implicit from game_state?)
+
+    if move_possible:
+        snake.insert(0, next_move)
+        snake.pop()
+    else:
+        reset_snake()
+
+
+def draw_blocks():
+    for r_index, row in enumerate(game_state):
+        for c_index, col in enumerate(row):
+            color = (0, 0, 0)
+            rect = pygame.Rect(
+                c_index * BLOCK_SIZE, r_index * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE
+            )
+            pygame.draw.rect(screen, color, rect)
+
+    for segment in snake:
+        color = (255, 0, 0)
+        rect = pygame.Rect(
+            segment[1] * BLOCK_SIZE, segment[0] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE
+        )
+        pygame.draw.rect(screen, color, rect)
+
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+        if event.type == pygame.KEYDOWN:
+            scancode = event.scancode
+            if scancode == 82:
+                direction_state = Direction.UP
+            elif scancode == 79:
+                direction_state = Direction.RIGHT
+            elif scancode == 81:
+                direction_state = Direction.DOWN
+            elif scancode == 80:
+                direction_state = Direction.LEFT
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    move_snake()
+    draw_blocks()
     pygame.display.flip()
+    clock.tick(FPS)
